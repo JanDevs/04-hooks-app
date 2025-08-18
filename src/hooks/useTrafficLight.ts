3c57f8d22026cb93a57833ca29db28466d22c865
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2025-08-18 16:09:59 -0600

    custom hook

---
+++ src/hooks/useTrafficLight.ts
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+
+const colors = {
+    red: 'bg-red-500 animate-pulse',
+    yellow: 'bg-yellow-500 animate-pulse',
+    green: 'bg-green-500 animate-pulse',
+}
+
+// type TrafficLightColor = 'red' | 'yellow' | 'green';
+type TrafficLightColor = keyof typeof colors;
+
+export const useTrafficLight = () => {
+  
+const [light, setLight] = useState<TrafficLightColor>('red');
+    const [countdown, setCountdown] = useState(5);
+    //Countdown
+    useEffect(() => {
+      if( countdown === 0 ) return;
+      //Se crea una constante para identificar el intervalo y así poderlo borrar
+      const intervalId = setInterval(() => {
+        // console.log('setInterval llamado')
+        setCountdown( prev => prev -1);
+      }, 1000);
+
+      //El return es la función de limpieza del useEffect, es lo que se hace tras ejecutar el código
+      return () => {
+        // console.log('Cleanup effect')
+        //clearInterval sirve para eliminar el intervalo y que no se stackeen ejecuciones
+        clearInterval(intervalId);
+      }
+ 
+    }, [countdown]);
+
+    //Cambio de luz
+
+    useEffect(() => {
+      if(countdown > 0) return;
+
+      if(countdown === 0 ){
+        setCountdown(5);
+        if(light === 'red'){
+          setLight('green');
+          return;
+        }
+        if(light === 'yellow'){
+          setLight('red');
+          return;
+        }
+        if(light === 'green'){
+          setLight('yellow');
+          return;
+        }
+      }
+    },[countdown, light]);
+
+    return {
+        light,
+        colors,
+        countdown
+    }
+
+}
+
