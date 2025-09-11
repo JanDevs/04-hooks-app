24b1031d92b674cf48e8675107f5d04dac28d5d3
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2025-09-11 10:22:15 -0600

    Notificación con sonner

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster, toast } from 'sonner';
 import './index.css'
 // import HooksApp from './HooksApp'
 // import { TrafficLight } from './01-useState/TrafficLight'
@@ -16,6 +17,7 @@ import { InstagromApp } from './07-useOptimistic/InstagromApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster />
     <InstagromApp />
   </StrictMode>,
 )
