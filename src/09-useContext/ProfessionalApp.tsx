761f56c19a7ceaefaaebdb26baefde8b1223601e
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2025-09-22 12:53:38 -0600

    Preparación sección useContext

---
+++ src/09-useContext/ProfessionalApp.tsx
@@ -0,0 +1,11 @@
+
+
+const ProfessionalApp = () => {
+  return (
+    <div className='bg-gradient flex flex-col'>
+      <h1>Professional App</h1>
+    </div>
+  )
+}
+
+export default ProfessionalApp
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import { StrictMode, Suspense } from 'react'
+import { StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
-import { Toaster, toast } from 'sonner';
+import { Toaster } from 'sonner';
 import './index.css'
 // import HooksApp from './HooksApp'
 // import { TrafficLight } from './01-useState/TrafficLight'
@@ -13,20 +13,21 @@ import './index.css'
 // import MemoHook from './06-memos/MemoHook'
 // import MemoCounter from './06-memos/MemoCounter'
 // import { InstagromApp } from './07-useOptimistic/InstagromApp'
-import ClientInformation from './08-use-suspense/ClientInformation';
-import { getUserAction } from './08-use-suspense/api/get-user.action';
+// import ClientInformation from './08-use-suspense/ClientInformation';
+// import { getUserAction } from './08-use-suspense/api/get-user.action';
+import ProfessionalApp from './09-useContext/ProfessionalApp';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Toaster />
-    <Suspense fallback={
+    {/* <Suspense fallback={
         <div className='bg-gradient flex flex-col'>
           <h1 className='text-2xl'>Cargando...</h1>
         </div>
       }>
       <ClientInformation getUser={ getUserAction(1000) } />
-    </Suspense>
-    
+    </Suspense> */}
+    <ProfessionalApp />
   </StrictMode>,
 )
